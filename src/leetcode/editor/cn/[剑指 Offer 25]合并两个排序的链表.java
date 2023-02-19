6b58fd1bf85caaa8459b1dd8d8f306ea9e00e1f3
qaz942956627 <942956627@qq.com>
2023-02-20 00:58:22 +0800

    链表

---
+++ src/leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
@@ -0,0 +1,56 @@
+package leetcode.editor.cn;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class SolutionJ25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head;
+
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+
+        ListNode res = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            } else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+
+        if (l1 == null) {
+            head.next = l2;
+        }else {
+            head.next = l1;
+        }
+
+        return res;
+
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
